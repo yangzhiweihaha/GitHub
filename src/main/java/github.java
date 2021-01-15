5bec69ed2d0e56678e09297e2a90e605bc7df35f
yangzhiwei <123.com>
2021-01-15 23:53:46 +0800

    修改了西方不变

--- src/main/java/github.java
+++ src/main/java/github.java
@@ -1,6 +1,6 @@
 public class github {
     public static void main(String[] args) {
         System.out.println("welcome github");
-        System.out.println("东方不败");
+        System.out.println("西方不败");
     }
 }
